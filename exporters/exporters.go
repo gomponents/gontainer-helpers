51016099a9bef2cda6598c044ab207162cf4efea
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-31 03:17:04 +0400

    added missing primitive types

--- exporters/exporters.go
+++ exporters/exporters.go
@@ -237,7 +237,15 @@ func (p PrimitiveTypeSliceExporter) Supports(v interface{}) bool {
 		reflect.Int16,
 		reflect.Int32,
 		reflect.Int64,
-		reflect.Uint:
+		reflect.Uint,
+		reflect.Uint8,
+		reflect.Uint16,
+		reflect.Uint32,
+		reflect.Uint64,
+		reflect.Float32,
+		reflect.Float64,
+		reflect.Complex64,
+		reflect.Complex128:
 		return true
 	}
 
