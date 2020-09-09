77005472f4f6b636950b4797117cb728d5b4627a
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-09-09 19:56:27 +0400

    refactor

--- env/env.go
+++ env/env.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-const envVarDoesnExist = "environment variable `%s` does not exist"
+const envVarDoesntExist = "environment variable `%s` does not exist"
 
 // MustGetInt returns environment variable if exists,
 // otherwise it returns second argument if given.
@@ -16,7 +16,7 @@ func MustGet(key string, def ...string) string {
 		if len(def) > 0 {
 			return def[0]
 		}
-		panic(fmt.Sprintf(envVarDoesnExist, key))
+		panic(fmt.Sprintf(envVarDoesntExist, key))
 	}
 	return val
 }
@@ -29,7 +29,7 @@ func MustGetInt(key string, def ...int) int {
 		if len(def) > 0 {
 			return def[0]
 		}
-		panic(fmt.Sprintf(envVarDoesnExist, key))
+		panic(fmt.Sprintf(envVarDoesntExist, key))
 	}
 	res, err := strconv.Atoi(val)
 	if err != nil {
