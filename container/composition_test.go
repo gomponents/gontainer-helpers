3140e16f095b2089c0a5ab4b98b2d203fb78138c
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-10-21 17:12:54 +0400

    tests

--- container/composition_test.go
+++ container/composition_test.go
@@ -1,7 +1,9 @@
 package container
 
 import (
+	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -77,5 +79,75 @@ func TestComposition(t *testing.T) {
 			AtomicParamContainer{},
 			AtomicTaggedContainer{},
 		)
+	})
+	t.Run("Print a composed interface", func(t *testing.T) {
+		listMethods := func(o interface{}) []string {
+			r := reflect.TypeOf(o)
+			if r.Kind() == reflect.Ptr {
+				r = r.Elem()
+			}
+
+			typeOf := func(t reflect.Type) string {
+				r := t.Name()
+				if t.PkgPath() != "" {
+					r = fmt.Sprintf(`"%s".%s`, t.PkgPath(), r)
+				}
+				if r == "" {
+					r = t.String()
+				}
+				return r
+			}
+
+			var methods []string
+
+			for i := 0; i < r.NumMethod(); i++ {
+				mr := r.Method(i)
+				var inputs []string
+				for j := 1; j < mr.Type.NumIn(); j++ {
+					inputs = append(inputs, typeOf(mr.Type.In(j)))
+				}
+
+				var outputs []string
+				for j := 0; j < mr.Type.NumOut(); j++ {
+					outputs = append(outputs, typeOf(mr.Type.Out(j)))
+				}
+				out := ""
+				switch len(outputs) {
+				case 0:
+				case 1:
+					out = fmt.Sprintf(" %s", outputs[0])
+				case 2:
+					out = fmt.Sprintf(" (%s)", strings.Join(outputs, ", "))
+				}
+
+				method := fmt.Sprintf(
+					"%s(%s)%s",
+					mr.Name,
+					strings.Join(inputs, ", "),
+					out,
+				)
+
+				methods = append(methods, method)
+			}
+
+			return methods
+		}
+
+		m := []string{"// service container"}
+		m = append(m, listMethods(AtomicContainer{})...)
+		m = append(m, "\n", "// param container")
+		m = append(m, listMethods(AtomicParamContainer{})...)
+		m = append(m, "\n", "// tagged container")
+		m = append(m, listMethods(AtomicTaggedContainer{})...)
+
+		i := `type Container interface {
+	%s
+}`
+		i = fmt.Sprintf(
+			i,
+			strings.Join(m, "\n\t"),
+		)
+
+		t.Log("\n" + i)
 	})
 }
