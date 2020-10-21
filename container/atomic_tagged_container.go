9f355b5cf11a23625d166dcbc3e412655dd50965
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-10-21 19:58:06 +0400

    tagged container

--- container/atomic_tagged_container.go
+++ container/atomic_tagged_container.go
@@ -1,10 +1,52 @@
 package container
 
+import (
+	"sync"
+)
+
 type taggedContainer interface {
+	GetByTag(string) ([]interface{}, error)
+	MustGetByTag(string) []interface{}
+	TagService(id string, tag string, priority int) error
+	OverrideTagService(id string, tag string, priority int)
+	IsTaggedBy(id string, tag string) bool
 }
 
 type AtomicTaggedContainer struct {
+	container taggedContainer
+	locker    sync.Locker
 }
 
-func (a *AtomicTaggedContainer) Get() {
+func NewAtomicTaggedContainer(container taggedContainer, locker sync.Locker) *AtomicTaggedContainer {
+	return &AtomicTaggedContainer{container: container, locker: locker}
+}
+
+func (a AtomicTaggedContainer) GetByTag(id string) ([]interface{}, error) {
+	a.locker.Lock()
+	defer a.locker.Unlock()
+	return a.GetByTag(id)
+}
+
+func (a AtomicTaggedContainer) MustGetByTag(id string) []interface{} {
+	a.locker.Lock()
+	defer a.locker.Unlock()
+	return a.MustGetByTag(id)
+}
+
+func (a AtomicTaggedContainer) TagService(id string, tag string, priority int) error {
+	a.locker.Lock()
+	defer a.locker.Unlock()
+	return a.TagService(id, tag, priority)
+}
+
+func (a AtomicTaggedContainer) OverrideTagService(id string, tag string, priority int) {
+	a.locker.Lock()
+	defer a.locker.Unlock()
+	a.OverrideTagService(id, tag, priority)
+}
+
+func (a AtomicTaggedContainer) IsTaggedBy(id string, tag string) bool {
+	a.locker.Lock()
+	defer a.locker.Unlock()
+	return a.container.IsTaggedBy(id, tag)
 }
