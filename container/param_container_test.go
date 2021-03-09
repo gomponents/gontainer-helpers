efd867996e9213fd4b59e7af1ce87181284a942e
bkrukowski <krukowski.bartlomiej@gmail.com>
2021-03-09 12:46:50 +0400

    tests

--- container/param_container_test.go
+++ container/param_container_test.go
@@ -2,12 +2,28 @@ package container
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParamContainer_GetParam(t *testing.T) {
 	t.Run("Given errors", func(t *testing.T) {
+		circularContainer := NewParamContainer(nil)
+		deps := map[string]string{
+			"username": "nickname",
+			"nickname": "name",
+			"name":     "username",
+		}
+		for k, dep := range deps {
+			d := dep
+			circularContainer.OverrideParam(k, ParamDefinition{
+				Provider: func() interface{} {
+					return circularContainer.MustGetParam(d)
+				},
+			})
+		}
+
 		scenarios := []struct {
 			container *ParamContainer
 			id        string
@@ -29,6 +45,11 @@ func TestParamContainer_GetParam(t *testing.T) {
 				container: NewParamContainer(nil),
 				id:        "db.host",
 				error:     "parameter `db.host` does not exist",
+			},
+			{
+				container: circularContainer,
+				id:        "nickname",
+				error:     "cannot get parameter `nickname`: cannot get parameter `name`: cannot get parameter `username`: circular dependency: nickname -> name -> username -> nickname",
 			},
 		}
 
