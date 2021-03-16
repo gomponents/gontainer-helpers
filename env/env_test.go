3d325f759c576de92eae0afc4f5416454dbdf52b
bkrukowski <krukowski.bartlomiej@gmail.com>
2021-03-16 15:57:22 +0400

    tests

--- env/env_test.go
+++ env/env_test.go
@@ -8,6 +8,59 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGet(t *testing.T) {
+	scenarios := []struct {
+		envs     map[string]string
+		key      string
+		def      []string
+		expected string
+		error    string
+	}{
+		{
+			key:   "MYSQL_USER",
+			error: "environment variable `MYSQL_USER` does not exist",
+		},
+		{
+			key:      "MYSQL_USER",
+			def:      []string{"root"},
+			expected: "root",
+		},
+		{
+			envs:     map[string]string{"MYSQL_USER": "mysql-user"},
+			key:      "MYSQL_USER",
+			expected: "mysql-user",
+		},
+	}
+
+	for i, s := range scenarios {
+		t.Run(fmt.Sprintf("Scenario #%d", i), func(t *testing.T) {
+			// prevent failure in case of existing environment variables
+			unsetEnvs(t, s.envs)
+			unsetEnvs(t, map[string]string{s.key: ""})
+
+			setEnvs(t, s.envs)
+			defer func() {
+				unsetEnvs(t, s.envs)
+			}()
+
+			v, err := Get(s.key, s.def...)
+
+			if s.error == "" {
+				assert.NoError(t, err)
+				assert.Equal(
+					t,
+					s.expected,
+					v,
+				)
+				return
+			}
+
+			assert.EqualError(t, err, s.error)
+			assert.Equal(t, "", v)
+		})
+	}
+}
+
 func TestGetInt(t *testing.T) {
 	t.Run("Given scenarios", func(t *testing.T) {
 		scenarios := []struct {
@@ -15,6 +68,7 @@ func TestGetInt(t *testing.T) {
 			key      string
 			def      []int
 			expected int
+			error    string
 		}{
 			{
 				envs:     map[string]string{"GONTAINER_KEY": "123"},
@@ -27,37 +81,6 @@ func TestGetInt(t *testing.T) {
 				def:      []int{500},
 				expected: 500,
 			},
-		}
-
-		for i, s := range scenarios {
-			t.Run(fmt.Sprintf("Scenario #%d", i), func(t *testing.T) {
-				// prevent failure in case of existing environment variables
-				unsetEnvs(t, s.envs)
-				unsetEnvs(t, map[string]string{s.key: ""})
-
-				setEnvs(t, s.envs)
-				defer func() {
-					unsetEnvs(t, s.envs)
-				}()
-
-				v, err := GetInt(s.key, s.def...)
-				assert.NoError(t, err)
-				assert.Equal(
-					t,
-					s.expected,
-					v,
-				)
-			})
-		}
-	})
-
-	t.Run("Given errors", func(t *testing.T) {
-		scenarios := []struct {
-			envs  map[string]string
-			key   string
-			def   []int
-			error string
-		}{
 			{
 				envs:  map[string]string{"GONTAINER_KEY": "hello"},
 				key:   "GONTAINER_KEY",
@@ -87,6 +110,17 @@ func TestGetInt(t *testing.T) {
 				}()
 
 				v, err := GetInt(s.key, s.def...)
+
+				if s.error == "" {
+					assert.NoError(t, err)
+					assert.Equal(
+						t,
+						s.expected,
+						v,
+					)
+					return
+				}
+
 				assert.EqualError(t, err, s.error)
 				assert.Equal(t, 0, v)
 			})
